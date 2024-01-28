a62df4e08e0e2d23f84f69ffdabab310ed7ed5b6
Anthony McKinney <amv493@gmail.com>
2024-01-28 12:47:07 -0500

    Basic setup for a quiz website

---
+++ src/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language", 
+        options: [
+            "A. Hyper Type Multi Language", 
+            "B. Hyper Text Multiple Language", 
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2, 
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet", 
+        options: [
+            "A. Cascading Style Sheet", 
+            "B. Cute Style Sheet", 
+            "C. Computer Style Sheet",
+            "D. Codeha Style Sheet"
+        ]
+    },
+    {
+        numb: 3, 
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Processor", 
+        options: [
+            "A. Hypertext Processor", 
+            "B. Hometext Programming", 
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language", 
+        options: [
+            "A. Strength Query Language", 
+            "B. Stylesheet Query Language", 
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5, 
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language", 
+        options: [
+            "A. Excellent Multiple Language", 
+            "B. Explore Multiple Language", 
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 6, 
+        question: "______ is the process of finding errors and fixing them within a program",
+        answer: "B. Debugging", 
+        options: [
+            "A. Compiling", 
+            "B. Debugging", 
+            "C. Executing",
+            "D. Scanning"
+        ]
+    },
+    {
+        numb: 7, 
+        question: "Which of the following is not a required part of a Java for loop?",
+        answer: "C. Variable", 
+        options: [
+            "A. Initialization", 
+            "B. Condition", 
+            "C. Variable",
+            "D. Increment"
+        ]
+    },
+    {
+        numb: 8, 
+        question: "A loop that never ends is reffered to as a(n) _______.",
+        answer: "B. Infinite loop", 
+        options: [
+            "A. While loop", 
+            "B. Infinite loop", 
+            "C. Recursive loop",
+            "D. for loop"
+        ]
+    },
+    {
+        numb: 9, 
+        question: "Which command will stop an infinite loop?",
+        answer: "D. Ctrl + C", 
+        options: [
+            "A. Alt + C", 
+            "B. Shift + C", 
+            "C. Esc",
+            "D. Ctrl + C"
+        ]
+    },
+    {
+        numb: 10, 
+        question: "If you're looking to create a repetition statment using a loop, and don't know the number of times each loop needs to execute, which conditional statement would you need to use?",
+        answer: "A. While loop", 
+        options: [
+            "A. While loop", 
+            "B. If-Else", 
+            "C. For loop",
+            "D. Switch statement"
+        ]
+    },
+]
